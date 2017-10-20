61e20c897a704238bda1ec3e8eb6fe2287b141bb
Kensuke Numakura <knmkr3gma@gmail.com>
2017-10-20 17:19:54 +0900

    0.1.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='genomelink',
-    version='0.1.1',
+    version='0.1.2',
     description='GENOMELINK',
     long_description='',
     author='AWAKENS',
