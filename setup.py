694ffc1d3e62d19662ba6ba2efe536c0661ccece
Kensuke Numakura <knmkr3gma@gmail.com>
2017-10-21 15:42:24 +0900

    fix. rename

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.rst')) as f:
+with open(os.path.join(here, 'README.md')) as f:
     long_description = f.read()
 
 setup(
