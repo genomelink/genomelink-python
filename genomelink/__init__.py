e46440af7e90a2d67f921115e0080a06139edb61
Kensuke Numakura <knmkr3gma@gmail.com>
2018-07-13 14:00:15 +0900

    switch api base for develop

--- genomelink/__init__.py
+++ genomelink/__init__.py
@@ -1,4 +1,4 @@
-api_base = 'https://genomelink.io'
-
+import os
+api_base = os.environ.get('GENOMELINK_API_BASE', 'https://genomelink.io')
 from genomelink.oauth import OAuth
 from genomelink.resource.report import Report
